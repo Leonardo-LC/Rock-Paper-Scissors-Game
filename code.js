1d2db63efb57ae27042b4c9aea26b9b6c140c561
Leonardo-LC <leonardolopescrz@gmail.com>
2024-09-22 12:11:57 -0300

    The function playGame was created. So now the project is completed.

--- code.js
+++ code.js
@@ -3,21 +3,29 @@ function getComputerChoice(value){
     return Math.floor(Math.random() * value)
 }
 
-if(getComputerChoice(3) === 0){
-    computerChoice = "Rock"
-} else if(getComputerChoice(3) === 1){
-    computerChoice = "Paper"
-} else{
-    computerChoice = "Scissors"
-}
-
 //human choice section
 function getHumanChoice(){
- return prompt("Choose between Rock, Paper and Scissors")
-}
+    return prompt("Choose between Rock, Paper and Scissors")
+   }
+
+let humanScore = 0
+let computerScore = 0
+
+
+
+function playRound(){
+
+    let computerChoice
+
+    if(getComputerChoice(3) === 0){
+        computerChoice = "Rock"
+    } else if(getComputerChoice(3) === 1){
+        computerChoice = "Paper"
+    } else{
+        computerChoice = "Scissors"
+    }
 
 let humanChoice = getHumanChoice().toLowerCase()
-
 
 if(humanChoice === "rock"){
     humanChoice = "Rock"
@@ -32,66 +40,60 @@ if(humanChoice === "rock"){
 console.log(`The computer chose ${computerChoice}`)
 console.log(`And your chose ${humanChoice}`)
 
-
-
-let humanScore = 0
-let computerScore = 0
-
-function playRound(humanChoice, computerChoice){
     if(humanChoice === "Rock" && computerChoice === "Rock"){
         console.log("That's a draw")
-            humanScore = humanScore + 1
-            computerScore = computerScore + 1
+            humanScore += 1
+            computerScore += 1
 
     } else if(humanChoice === "Rock" && computerChoice === "Paper"){
         console.log("Paper beats Rock, so the computer won this round")
-            computerScore = computerScore + 3
+            computerScore += 3
 
     }  else if(humanChoice === "Rock" && computerChoice === "Scissors"){
         console.log("Rock beats Scissors, so the you won this round")
-            humanScore = humanScore + 3
+            humanScore += 3
 
     } else if(humanChoice === "Paper" && computerChoice === "Rock"){
         console.log("Paper beats Rock, so you won this round")
-            humanScore = humanScore + 3
+           humanScore += 3
 
     }  else if(humanChoice === "Paper" && computerChoice === "Paper"){
         console.log("That's a draw")
-            humanScore = humanScore + 1
-            computerScore = computerScore +1
+            humanScore += 1
+            computerScore += 1
 
     }  else if(humanChoice === "Paper" && computerChoice === "Scissors"){
         console.log("Scissors beats Paper, so the computer won this round")
-            computerScore = computerScore + 3
+            computerScore += 3
 
     } else if(humanChoice === "Scissors" && computerChoice === "Rock"){
         console.log("Rock beats Scissors, so the computer won this round")
-            computerScore = computerScore + 3
+           computerScore += 3
 
     } else if(humanChoice === "Scissors" && computerChoice === "Paper"){
         console.log("Scissors beats Paper, so you won this round")
-            humanScore = humanScore + 3
+            humanScore += 3
 
     } else if(humanChoice === "Scissors" && computerChoice === "Scissors"){
         console.log("That's a draw")
-            humanScore = humanScore + 1
-            computerScore = computerScore +1
+        humanScore += 1
+        computerScore += 1
     }
 
-    return
+  
 
 }
 
-/*
-function playGame(){
-    for(let round = 0; round < 4; round++){
-        playRound(humanChoice, computerChoice)
-    }
+for (let i = 1; i <=5; i++){
+    console.log(`Round ${i}`);
+    playRound();
+    console.log(`Your score is ${humanScore} and the computer's score is ${computerScore}`); 
 }
-*/    
 
-//playGame()
-(playRound(humanChoice, computerChoice))
-console.log(`Your score is ${humanScore} and the computer score is ${computerScore}`)
+if(humanScore > computerScore){
+    console.log("Congratulations! You're the winner")
+} else{
+    console.log("Better luck next time")
+}
 
 
